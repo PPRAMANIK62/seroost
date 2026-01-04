398c32d4d74efd04fc2a591bce4375edb60d2b9e
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2026-01-04 17:48:32 +0530

    indexing directories into json file

---
+++ src/main.rs
@@ -0,0 +1,141 @@
+use std::{
+    collections::HashMap,
+    fs::{File, read_dir},
+    io::Result,
+    path::{Path, PathBuf},
+};
+use xml::{EventReader, reader::XmlEvent};
+
+#[derive(Debug)]
+struct Lexer<'a> {
+    content: &'a [char],
+}
+
+impl<'a> Lexer<'a> {
+    fn new(content: &'a [char]) -> Self {
+        Self { content }
+    }
+
+    fn trim_left(&mut self) {
+        while self.content.len() > 0 && self.content[0].is_whitespace() {
+            self.content = &self.content[1..];
+        }
+    }
+
+    fn chop(&mut self, n: usize) -> &'a [char] {
+        let token = &self.content[0..n];
+        self.content = &self.content[n..];
+        token
+    }
+
+    fn chop_while<P: FnMut(&char) -> bool>(&mut self, mut predicate: P) -> &'a [char] {
+        let mut n = 0;
+        while n < self.content.len() && predicate(&self.content[n]) {
+            n += 1;
+        }
+        self.chop(n)
+    }
+
+    fn next_token(&mut self) -> Option<&'a [char]> {
+        // trim white spaces from the left
+        self.trim_left();
+        if self.content.len() == 0 {
+            return None;
+        }
+
+        if self.content[0].is_numeric() {
+            return Some(self.chop_while(|x| x.is_numeric()));
+        }
+
+        if self.content[0].is_alphabetic() {
+            return Some(self.chop_while(|x| x.is_alphabetic()));
+        }
+
+        Some(self.chop(1))
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = &'a [char];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next_token()
+    }
+}
+
+fn index_document(content: &str) -> HashMap<String, usize> {
+    todo!("not implemented");
+}
+
+fn read_entire_xml_file<P: AsRef<Path>>(file_path: P) -> Result<String> {
+    let file = File::open(file_path)?;
+    let event_reader = EventReader::new(file);
+    let mut content = String::new();
+
+    for event in event_reader.into_iter() {
+        if let XmlEvent::Characters(text) = event.expect("TODO") {
+            content.push_str(&text);
+            content.push(' ');
+        }
+    }
+    Ok(content)
+}
+
+type TermFreq = HashMap<String, usize>;
+type TermFreqIndex = HashMap<PathBuf, TermFreq>;
+
+fn main() -> Result<()> {
+    let index_path = "index.json";
+    let index_file = File::open(index_path)?;
+    println!("Reading {index_path} index file...");
+    let tf_index: TermFreqIndex = serde_json::from_reader(index_file).expect("serde works fine");
+    println!(
+        "{index_path} contains {count} files",
+        count = tf_index.len()
+    );
+
+    Ok(())
+}
+
+fn main2() -> Result<()> {
+    let dir_path = "docs.gl/gl4";
+    let dir = read_dir(dir_path)?;
+    let mut term_freq_index = TermFreqIndex::new();
+
+    for file in dir {
+        let file_path = file?.path();
+        println!("Indexing {:?}...", &file_path);
+
+        let content = read_entire_xml_file(&file_path)?
+            .chars()
+            .collect::<Vec<_>>();
+
+        let mut term_frequency = TermFreq::new();
+
+        for token in Lexer::new(&content) {
+            let term = token
+                .iter()
+                .map(|x| x.to_ascii_uppercase())
+                .collect::<String>();
+
+            if let Some(freq) = term_frequency.get_mut(&term) {
+                *freq += 1;
+            } else {
+                term_frequency.insert(term, 1);
+            }
+        }
+
+        let mut stats = term_frequency.iter().collect::<Vec<_>>();
+        stats.sort_by_key(|(_, f)| *f);
+        stats.reverse();
+
+        term_freq_index.insert(file_path, term_frequency);
+    }
+
+    let index_path = "index.json";
+    println!("Saving {index_path}...");
+    let index_file = File::create(index_path)?;
+    serde_json::to_writer(index_file, &term_freq_index).expect("serde works fine");
+
+    Ok(())
+}
