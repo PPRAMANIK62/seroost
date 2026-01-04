c317efd699eed8f3933ce28adce9084885c42ce4
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2026-01-04 19:31:57 +0530

    fix: don't stop indexing just because we failed to parse one file

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,11 @@ use std::{
     path::{Path, PathBuf},
     process::exit,
 };
-use xml::{EventReader, reader::XmlEvent};
+use xml::{
+    EventReader,
+    common::{Position, TextPosition},
+    reader::XmlEvent,
+};
 
 struct Lexer<'a> {
     content: &'a [char],
@@ -64,18 +68,36 @@ impl<'a> Iterator for Lexer<'a> {
     }
 }
 
-fn read_entire_xml_file<P: AsRef<Path>>(file_path: P) -> Result<String> {
-    let file = File::open(file_path)?;
+fn parse_entire_xml_file(file_path: &Path) -> Option<String> {
+    let file = File::open(&file_path)
+        .map_err(|err| {
+            eprint!(
+                "ERROR: could not open file {file_path}: {err}",
+                file_path = file_path.display()
+            )
+        })
+        .ok()?;
     let event_reader = EventReader::new(file);
     let mut content = String::new();
 
     for event in event_reader.into_iter() {
-        if let XmlEvent::Characters(text) = event.expect("TODO") {
+        let event = event
+            .map_err(|err| {
+                let TextPosition { row, column } = err.position();
+                let msg = err.msg();
+                eprint!(
+                    "{file_path}:{row}:{column}: ERROR: {msg}",
+                    file_path = file_path.display()
+                );
+            })
+            .ok()?;
+
+        if let XmlEvent::Characters(text) = event {
             content.push_str(&text);
-            content.push(' ');
+            content.push_str(" ");
         }
     }
-    Ok(content)
+    Some(content)
 }
 
 type TermFreq = HashMap<String, usize>;
@@ -97,13 +119,14 @@ fn index_folder(dir_path: &str) -> Result<()> {
     let dir = read_dir(dir_path)?;
     let mut term_freq_index = TermFreqIndex::new();
 
-    for file in dir {
+    'next_file: for file in dir {
         let file_path = file?.path();
         println!("Indexing {:?}...", &file_path);
 
-        let content = read_entire_xml_file(&file_path)?
-            .chars()
-            .collect::<Vec<_>>();
+        let content = match parse_entire_xml_file(&file_path) {
+            Some(content) => content.chars().collect::<Vec<_>>(),
+            None => continue 'next_file,
+        };
 
         let mut term_frequency = TermFreq::new();
 
