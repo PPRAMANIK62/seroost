311cd62e792e0b76e9d085287a20ad2c7d86b025
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2026-01-22 23:11:13 +0530

    refactor: model module

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use std::{
-    collections::HashMap,
     env,
     fs::{File, read_dir},
-    path::{Path, PathBuf},
+    path::Path,
     process::ExitCode,
 };
 use tiny_http::{Header, Method, Request, Response, Server, StatusCode};
@@ -11,70 +10,8 @@ use xml::{
     common::{Position, TextPosition},
     reader::XmlEvent,
 };
-
-struct Lexer<'a> {
-    content: &'a [char],
-}
-
-impl<'a> Lexer<'a> {
-    fn new(content: &'a [char]) -> Self {
-        Self { content }
-    }
-
-    fn trim_left(&mut self) {
-        while !self.content.is_empty() && self.content[0].is_whitespace() {
-            self.content = &self.content[1..];
-        }
-    }
-
-    fn chop(&mut self, n: usize) -> &'a [char] {
-        let token = &self.content[0..n];
-        self.content = &self.content[n..];
-        token
-    }
-
-    fn chop_while<P: FnMut(&char) -> bool>(&mut self, mut predicate: P) -> &'a [char] {
-        let mut n = 0;
-        while n < self.content.len() && predicate(&self.content[n]) {
-            n += 1;
-        }
-        self.chop(n)
-    }
-
-    fn next_token(&mut self) -> Option<String> {
-        // trim white spaces from the left
-        self.trim_left();
-        if self.content.is_empty() {
-            return None;
-        }
-
-        // for only numbers -> starting with number & ending with number
-        if self.content[0].is_numeric() {
-            return Some(self.chop_while(|x| x.is_numeric()).iter().collect());
-        }
-
-        // starting with alphabet & ending with number
-        if self.content[0].is_alphabetic() {
-            return Some(
-                self.chop_while(|x| x.is_alphanumeric())
-                    .iter()
-                    .map(|x| x.to_ascii_uppercase())
-                    .collect(),
-            );
-        }
-
-        // rest all single char
-        Some(self.chop(1).iter().collect())
-    }
-}
-
-impl<'a> Iterator for Lexer<'a> {
-    type Item = String;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.next_token()
-    }
-}
+mod model;
+use model::*;
 
 fn parse_entire_xml_file(file_path: &Path) -> Result<String, ()> {
     let file = File::open(file_path).map_err(|err| {
@@ -103,9 +40,6 @@ fn parse_entire_xml_file(file_path: &Path) -> Result<String, ()> {
     }
     Ok(content)
 }
-
-type TermFreq = HashMap<String, usize>;
-type TermFreqIndex = HashMap<PathBuf, TermFreq>;
 
 fn save_term_frequency_index(tf_index: &TermFreqIndex, index_path: &str) -> Result<(), ()> {
     println!("Saving {index_path}...");
@@ -174,21 +108,6 @@ fn term_frequency_index_of_folder(dir_path: &Path, tf_index: &mut TermFreqIndex)
 
 // TODO: Precache as much of tf-idf values as possible during indexing
 // TODO: Use sqlite3 to store the index
-fn tf(term: &str, document: &TermFreq) -> f32 {
-    let a = document.get(term).cloned().unwrap_or(0) as f32;
-    let b = document.iter().map(|(_, f)| *f).sum::<usize>() as f32;
-    a / b
-}
-
-fn idf(term: &str, document: &TermFreqIndex) -> f32 {
-    let n = document.len() as f32;
-    let m = document
-        .values()
-        .filter(|tf| tf.contains_key(term))
-        .count()
-        .max(1) as f32;
-    (n / m).log10()
-}
 
 fn usage(program: &str) {
     eprintln!("Usage: {program} [subcommand] [OPTIONS]");
@@ -218,21 +137,6 @@ fn serve_404(request: Request) -> Result<(), ()> {
     request
         .respond(Response::from_string("404").with_status_code(StatusCode(404)))
         .map_err(|err| eprintln!("ERROR: could not serve a request: {err}"))
-}
-
-fn search_query<'a>(tf_index: &'a TermFreqIndex, query: &'a [char]) -> Vec<(&'a Path, f32)> {
-    let mut result = Vec::<(&Path, f32)>::new();
-    let tokens = Lexer::new(&query).collect::<Vec<_>>();
-    for (path, tf_table) in tf_index {
-        let mut rank = 0f32;
-        for token in &tokens {
-            rank += tf(&token, &tf_table) * idf(&token, &tf_index);
-        }
-        result.push((path, rank));
-    }
-    result.sort_by(|(_, rank1), (_, rank2)| rank1.partial_cmp(rank2).unwrap());
-    result.reverse();
-    result
 }
 
 fn serve_api_search(tf_index: &TermFreqIndex, mut request: Request) -> Result<(), ()> {
